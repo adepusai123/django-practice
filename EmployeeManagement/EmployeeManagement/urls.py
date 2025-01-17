533d2ce6c96fcce057e668a5f82f763da1ecad25
Sai Kumar <adepusai123@gmail.com>
2025-01-17 18:27:57 +0530

    home page updated

--- EmployeeManagement/EmployeeManagement/urls.py
+++ EmployeeManagement/EmployeeManagement/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from SkillSet import views as skillsetviews
 
 urlpatterns = [
+    path('', skillsetviews.homepage),
     path('admin/', admin.site.urls),
 ]
